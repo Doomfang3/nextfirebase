7a16064cc62bd3bf95b27ddcbc60cab6f19d6095
Doomfang3 <saubinmathieu@gmail.com>
2021-08-14 11:42:37 +0200

    Auth done

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import { Toaster } from "react-hot-toast";
 import Navbar from "../components/Navbar";
 import { UserContext } from "../lib/context";
+import { useUserData } from "../lib/hooks";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const userData = useUserData();
+
   return (
-    <UserContext.Provider value={{ user: {}, username: "Mat" }}>
+    <UserContext.Provider value={{ ...userData }}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
