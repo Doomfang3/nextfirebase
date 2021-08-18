d426a748fe1403a3b42639f57077999061aa2f5b
Doomfang3 <saubinmathieu@gmail.com>
2021-08-18 13:24:14 +0200

    Right before react-hook-form codemon

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,5 +1,86 @@
+import { useCollection } from "react-firebase-hooks/firestore";
+import kebabCase from "lodash.kebabcase";
+import { useRouter } from "next/dist/client/router";
+
+import AuthCheck from "../../components/AuthCheck";
+import { auth, firestore, serverTimestamp } from "../../lib/firebase";
+import PostFeed from "../../components/PostFeed";
+import { useContext, useState } from "react";
+import { UserContext } from "../../lib/context";
+import toast from "react-hot-toast";
+
 const Admin = () => {
-  return <main>Admin</main>;
+  return (
+    <main>
+      <AuthCheck>
+        <PostList />
+        <CreateNewPost />
+      </AuthCheck>
+    </main>
+  );
+};
+
+const PostList = () => {
+  const ref = firestore.collection("users").doc(auth.currentUser.uid).collection("posts");
+  const query = ref.orderBy("createdAt");
+  const [querySnapshot] = useCollection(query);
+
+  const posts = querySnapshot?.docs.map(doc => doc.data());
+
+  return (
+    <>
+      <h1>Manage your Posts</h1>
+      <PostFeed posts={posts} admin />
+    </>
+  );
+};
+
+const CreateNewPost = () => {
+  const router = useRouter();
+  const { username } = useContext(UserContext);
+  const [title, setTitle] = useState("");
+
+  // Ensure slug is URL safe
+  const slug = encodeURI(kebabCase(title));
+
+  const isValid = title.length > 3 && title.length < 100;
+
+  const createPost = async event => {
+    event.preventDefault();
+    const uid = auth.currentUser.uid;
+    const ref = firestore.collection("users").doc(uid).collection("posts").doc(slug);
+    const data = {
+      title,
+      slug,
+      uid,
+      username,
+      published: false,
+      content: "# Jelou World",
+      createdAt: serverTimestamp(),
+      updatedAt: serverTimestamp(),
+      heartCount: 0,
+    };
+
+    await ref.set(data);
+    toast.success("Post created!");
+    router.push(`/admin/${slug}`);
+  };
+
+  return (
+    <form onSubmit={createPost}>
+      <input
+        value={title}
+        onChange={event => setTitle(event.target.value)}
+        placeholder='My Awesome Article!'
+      />
+      <p>
+        <strong>Slug:</strong> {slug}
+      </p>
+      <button type='submit' disabled={!isValid} className='btn-green'>
+        Create New Post
+      </button>
+    </form>
+  );
 };
 
 export default Admin;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,54 @@
 import toast from "react-hot-toast";
+import { firestore, fromMillis, postToJSON } from "../lib/firebase";
+import PostFeed from "../components/PostFeed";
+import Loader from "../components/Loader";
+import { useState } from "react";
 
-export default function Home() {
+const LIMIT = 1;
+
+export async function getServerSideProps(context) {
+  const postsQuery = firestore
+    .collectionGroup("posts")
+    .where("published", "==", true)
+    .orderBy("createdAt", "desc")
+    .limit(LIMIT);
+
+  const posts = (await postsQuery.get()).docs.map(postToJSON);
+
+  return {
+    props: { posts },
+  };
+}
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [isLoading, setIsLoading] = useState(false);
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  const getMorePosts = async () => {
+    setIsLoading(true);
+    const last = posts[posts.length - 1];
+    const cursor = typeof last.createdAt === "number" ? fromMillis(last.createdAt) : last.createdAt;
+    const query = firestore
+      .collectionGroup("posts")
+      .where("published", "==", true)
+      .orderBy("createdAt", "desc")
+      .startAfter(cursor)
+      .limit(LIMIT);
+    const newPosts = (await query.get()).docs.map(doc => doc.data());
+    setPosts(posts.concat(newPosts));
+    setIsLoading(false);
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  };
+
   return (
-    <div>
-      {" "}
-      <button onClick={() => toast.success("hello toast!")}>Toast Me</button>
-    </div>
+    <main>
+      <PostFeed posts={posts} />
+      {!isLoading && !postsEnd && <button onClick={getMorePosts}>Load more</button>}
+      <Loader show={isLoading} />
+      {postsEnd && "You have reached the end!"}
+    </main>
   );
 }
